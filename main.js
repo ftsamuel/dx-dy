c53f518d5cf633a4b7523f639e52221a51ab25d2
ftsamuel <famakinde70@gmail.com>
2025-03-22 10:10:24 -0700

    chore: changed the comma :octocat:

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var  mobileLink = document.querySelector(".mobile_link_holder");
 var backdrop = document.querySelector(".backdrop")
 
 function dosomething(){
-    hamburgerdiv.classList.toggle("showburger")
-   mobileLink.classList.toggle("show_mobile_link_holder")
-   backdrop.classList.toggle("show_backdrop")
+    hamburgerdiv.classList.toggle("showburger");
+   mobileLink.classList.toggle("show_mobile_link_holder");
+   backdrop.classList.toggle("show_backdrop");
 }
 
