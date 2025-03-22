37513c61f424daab08cda60e6495c8b64a7ad430
ftsamuel <famakinde70@gmail.com>
2025-03-22 10:00:06 -0700

    feat: setup files :octocat:

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log("document")
+
+var hamburgerdiv = document.querySelector(".hamburger");
+var  mobileLink = document.querySelector(".mobile_link_holder");
+var backdrop = document.querySelector(".backdrop")
+
+function dosomething(){
+    hamburgerdiv.classList.toggle("showburger")
+   mobileLink.classList.toggle("show_mobile_link_holder")
+   backdrop.classList.toggle("show_backdrop")
+}
+
